7da3c763c02b3df7e95bce4e42e071d490f3d096
Marcel Munevar <marcelmunevar@gmail.com>
2026-03-12 09:32:13 -0400

    removed credit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,20 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="">
+    <footer className="bg-gray-200 py-6">
       <div className="container mx-auto flex gap-8 flex-col">
-        <p className="text-sm text-center p-4">
-          Image Source:{" "}
-          <a className="text-red-600" href="http://www.wocintechchat.com/">
-            WOCinTechChat
-          </a>
-          ,{" "}
-          <a
-            className="text-red-600"
-            href="https://www.iconfinder.com/iconsets/elpis"
-          >
-            Icon Finder
-          </a>
-        </p>
         <div className="flex flex-col md:flex-row text-center justify-between p-4">
           <p>Trevor Renson, Enrolled Agent</p>
           <p>All rights reserved</p>
